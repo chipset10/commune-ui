dc304a38b8bc66a8c328c7ea6dfc4c30fbb11ffb
chipset10 <adonis.j0101@gmail.com>
2024-03-12 15:33:55 +0800

    Initial Commit Wiget core repo

---
+++ packages/commune-widget-core/vite.config.ts
@@ -0,0 +1,23 @@
+/// <reference types="vitest" />
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [dts({ rollupTypes: true })],
+  base: '/',
+  build: {
+    sourcemap: true,
+    outDir: 'build',
+    lib: {
+      entry: 'src/index.ts',
+      formats: ['es']
+    }
+  },
+  test: {
+    environment: 'happy-dom',
+    setupFiles: ['./tests/unit/setup.ts'],
+    include: ['**/*.test.ts'],
+    exclude: ['**/node_modules/**', '**/dist/**', '**/build/**']
+  }
+});
